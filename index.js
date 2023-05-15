dd1eb858db57c60a0045f286670f08aa510f530a
Bradley Troll <bradleytroll@gmail.com>
2023-05-15 17:03:12 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(addy) {
+    const hq = 42
+    if (addy - hq < 0) {
+        return ((addy - hq) * -1)
+    }
+    return addy - hq
+};
+function distanceFromHqInFeet(addy) {
+    const newDist = distanceFromHqInBlocks(addy)
+    return newDist * 264;
+}; 
+function distanceTravelledInFeet(start, destination) {
+    const distance = start - destination 
+    if (start - destination < 0) {
+        return ((start - destination) * -264)     
+        }
+        return distance * 264
+    }
+
+function calculatesFarePrice(start, destination) {
+    const fare = distanceTravelledInFeet(start, destination) 
+    if (fare <= 400) {
+        return 0
+    }
+    else if (fare <= 2000) {
+        return ((fare - 400) * .02)
+    }
+    else if (fare <= 2500) {
+        return 25
+    }
+    else if (fare > 2500 ) {
+        return "cannot travel that far"
+    }
+    return fare
+}
